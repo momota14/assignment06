41a90cba7bd34ef9a33d3af8b19a86bf9bd075d1
Momota khatun <momotakhatun014@gmail.com>
2023-07-11 12:42:30 +0600

    import issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
-import Content from "./components/content";
-import Footer from "./components/footer";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
